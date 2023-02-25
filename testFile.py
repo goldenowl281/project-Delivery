1b2bd9e5fefa5972fa622bb707cd6955c474b18e
goldenowl281 <119384110+goldenowl281@users.noreply.github.com>
2023-02-25 15:10:21 +0630

    Update testFile.py

--- testFile.py
+++ testFile.py
@@ -1,23 +1,26 @@
 import delivery
 
 def sign_upAccount():
-    user = {}
+#     user = {}
     
-    user["name"] = input("Enter your name: ")
-    user["password"] = input("Enter your password: ")
+#     user["name"] = input("Enter your name: ")
+#     user["password"] = input("Enter your password: ")
     
-    confirm_password = input("Enter your confirm password: ")
-    if user["password"] != confirm_password:
-           return "Passwords do not match"
+#     confirm_password = input("Enter your confirm password: ")
+#     if user["password"] != confirm_password:
+#            return "Passwords do not match"
         
     
-    user["phone_number"] = input("Enter your phone number: ")
+#     user["phone_number"] = input("Enter your phone number: ")
     
-    obj = delivery.mongodatabase()
-    obj.save_user(user)
+#     obj = delivery.mongodatabase()
+#     obj.save_user(user)
 
-    print("User signed up successfully!")
+#     print("User signed up successfully!")
+     user = "Enter user_name--:#Enter your password--:#Enter your phone number--:"
+     return user
     
 
-
-    
+def loginAcc():
+     user1 = "Enter Ur name--:#Enter your password--:#Enter your phone number--:"
+     return user1
